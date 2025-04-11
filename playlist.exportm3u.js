048529b9b39daa8f9c0c981a03f3a771de5d674f
David Sahlström <david@madon.io>
2025-04-12 01:43:31 +0200

    feat: add parent playlist name in output name when it exists

--- playlist.exportm3u.js
+++ playlist.exportm3u.js
@@ -1,13 +1,20 @@
 const oldLocation = _settings["Music Folder Path"];
 const newLocation = _settings["New Folder Path"];
 const playlists = _vars.playlistsSelected;
+const allPlaylists = _vars.playlistsAll;
 
 if (!playlists) {
   throw new Error(`No playlists selected.`);
 }
 
 for (const playlist of playlists) {
-  let playlistData = `\n###STARTM3U ${playlist.name}\n`;
+  let playlistName = playlist.name;
+  // If the playlist is not a root playlist, build name from parent names.
+  if (playlist.parentId !== 1) {
+    playlistName = buildPlaylistName(playlist);
+  }
+
+  let playlistData = `\n###STARTM3U ${playlistName}\n`;
   playlistData += `#EXTM3U\n\n`;
   playlist.tracks = await playlist.getTracks();
 
@@ -27,4 +34,22 @@ for (const playlist of playlists) {
 
   // Write m3u data to log.
   _helpers.Log(`${playlistData}`);
+}
+
+function getParentPlaylistName(playlistId) {
+  const parentPlaylist = allPlaylists.find((p) => p.id === playlistId);
+  if (!parentPlaylist) {
+    return "";
+  }
+  const grandParentName = getParentPlaylistName(parentPlaylist.parentId);
+  return grandParentName
+    ? `${grandParentName} - ${parentPlaylist.name}`
+    : parentPlaylist.name;
+}
+
+function buildPlaylistName(playlist) {
+  const parentPlaylistName = getParentPlaylistName(playlist.parentId);
+  return parentPlaylistName
+    ? `${parentPlaylistName} - ${playlist.name}`
+    : playlist.id;
 }
